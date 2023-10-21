5c2baf97c375b8fa6ad71dfb3ab2265e14966fc2
neil tarar <neiltarar@gmail.com>
2023-10-21 15:04:26 +1100

    Adds db settings for the deployment

--- api/db/db.js
+++ api/db/db.js
@@ -1,14 +1,30 @@
-const {Pool} = require('pg');
+const { Pool } = require('pg');
 
-console.log(process.env.DB_PASSWORD);
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
-});
+let poolConfig;
+
+if (process.env.DATABASE_URL) {
+    poolConfig = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    };
+} else {
+    poolConfig = {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME ,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT
+    };
+
+    if (process.env.DEV_DB_PASSWORD) {
+        poolConfig.password = process.env.DEV_DB_PASSWORD;
+    }
+}
+
+const pool = new Pool(poolConfig);
 
 module.exports = {
     query: (text, params) => pool.query(text, params)
-}
+};
